fd3fb460978246cc8f43f60419c1691f3457323d
aayush9767 <89067887+aayush9767@users.noreply.github.com>
2021-08-20 14:18:26 +0530

    Create Jpr1.java

---
+++ Jpr1.java
@@ -0,0 +1,17 @@
+package com.Training;
+
+public class Jpr1 {
+
+	public static void main(String[] args) {
+		System.out.println("Maruti"+" maruti@gmail.com"+" P03150"+" 9759394251");
+        System.out.println("Arjun"+" arjun@gmail.com"+" P03151"+" 9185431798");
+        System.out.println("Vishal"+" vishal@gmail.com"+" P03152"+" 9963135214");
+        System.out.println("Varun"+" varun@gmail.com"+" P03153"+" 860025618");
+        System.out.println("Shlok"+" Shlok@gmail.com"+" P03154"+" 9274861239");
+
+
+	
+	}
+
+	
+}
